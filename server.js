42f068b3a571dd1cbfb1ba1b3e49e5232522a15b
Harish B <Harish@TrivSoft.com>
2017-05-26 15:58:44 +0530

    Edited server.js to support dynamic page loading

--- server.js
+++ server.js
@@ -5,22 +5,92 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
+var Articles = {
+	'ArticleOneContent' : {
+		title:"Article One By Harish", 
+		heading:"Article One", 
+		date:"25 May 2017", 
+		Content:`The market witnessed short covering move after concerns over possible rate hike by the US Federal Reserve were abated which forced short-sellers to cover their positions.
+	Latest FOMC minutes highlighted that policymakers at US Federal Reserve agreed that they should hold off on raising interest rates until it was clear that a recent US economic slowdown was temporary.`
+	}, 
+	'ArticleTwoContent' : {
+		title:"Article Tow By Gireesh", 
+		heading:"Article Two", 
+		date:"26 May 2017", 
+		Content:`Article Content
+	Prepared by Gireesh`
+	}, 
+	'ArticleThreeContent' : {
+		title:"Article One By Vinesh", 
+		heading:"Article Three", 
+		date:"27 May 2017", 
+		Content:`Article Content
+	Prepared by Vinesh`
+	}
+};
 
+function createTemplate(data){
+	var title = data.title;
+	var heading=data.heading;
+	var date = data.date;
+	var content = data.Content;
+	
+	var html_template = `
+	<html>
+		<head>
+			<title>
+				${title}
+			</title>
+			<meta name="viewport" content="width=device-width, initial-scale=1"> 
+			<link href="/ui/style.css" rel="stylesheet" />
+		</head>
+		<body>
+			<div class = "Container">
+				<div>
+					<a href='/'>Home</a>
+				</div>	
+				<hr/>
+				<h3>
+				${heading}
+				</h3>
+				<div>
+				${date}
+				</div>
+				<div>
+					${content}
+				</div>
+			</div>				
+		</body>
+	</html>
+	`
+	
+return html_template;
+}
+
+
+/*
 app.get('/article-one', function(req, res) {
-	res.send("Article one will be server from here");
+	//res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+	res.send(createTemplate(ArticleOneContent));
 });
 
 app.get('/article-two', function(req, res) {
-	res.send("Article two will be server from here");
+	res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three', function(req, res) {
-	res.send("Article three will be server from here");
+	res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+});
+*/
+
+app.get('/:In_ArticleName', function(req, res) {
+	//var LocalArticleName = req.params.In_ArticleName;
+	res.send(createTemplate(Articles[req.params.In_ArticleName]));
 });
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
